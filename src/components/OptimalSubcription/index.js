a9befdc0f07c71015e2b01a25fc8ba27c35dd3ec
maksymPtashnyk <super.zet65@gmail.com>
2024-04-19 16:52:58 +0300

    Added Optimal Subcription and Gifts screens

--- src/components/OptimalSubcription/index.js
+++ src/components/OptimalSubcription/index.js
@@ -1,32 +1,26 @@
 import styles from './index.module.scss'
 
-const features = [{
-  img: 'feature-icon-1.png',
-  text: "Самый большой выбор техники"
-},
-{
-  img: 'feature-icon-2.png',
-  text: "Быстрое оформление заказа"
-},
-{
-  img: 'feature-icon-3.png',
-  text: "Можно забрать заказ в пункте выдачи"
-},
-{
-  img: 'feature-icon-4.png',
-  text: "Доставка в любую точку Москвы"
-},
-{
-  img: 'feature-icon-5.png',
-  text: "Оплата картой и наличными"
-},
-
-]
 
 export const OptimalSubcription = () => {
   return (
-    <section className={styles.feature}>
-
+    <section className={styles.optimal}>
+      <div className={styles.optimal__overlay}></div>
+      <div className={styles.optimal__content}>
+        <article className={styles.optimal__title}>
+          <p>Передплата</p>
+          <h3>Оптимальна</h3>
+        </article>
+        <article className={styles.optimal__description}>
+          <h2>10000 фільмів та 160+ каналів</h2>
+          <p>Голлівудська класика та свіжі <span>кіно-новинки</span> за підпискою, прямі футбольні трансляції та понад 160 каналів</p>
+        </article>
+        <article className={styles.optimal__try}>
+          <h2>Від 1 грн</h2>
+          <button className={styles.optimal__try__button}>cпробувати</button>
+          <p>Перші 14 днів за 1 грн, <br/> далі – 97 грн/міс.</p>
+          <button className={styles.optimal__try__details}>детальніше</button>
+        </article>
+      </div>
     </section>
   )
 }
